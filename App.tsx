1dbf543be8f57330489185b9e33ecaf4283488ad
Lucas Vieira <lucas130408@gmail.com>
2026-04-24 20:53:19 -0300

    feat: Home and Logo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Home } from "./src/screens/Home/Home"
 import { Footer } from "./src/components/Footer/Footer";
 import { ContactButton } from "./src/components/ContactButton/ContactButton";
 
 export default function App() {
   return (
     <View>
-      <Footer/>
+    <Home/>
        <ContactButton />
     </View>
   );
